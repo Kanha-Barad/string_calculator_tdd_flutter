df4a25acc8640ca0bf162568ef15c3dead3789e5
SLIMS-MOBILE-APPS <slimsmobileapps@gmail.com>
2025-08-12 17:49:49 +0530

    test: add failing test for two comma-separated numbers

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,3 +1,13 @@
-test('Single number returns value', () {
-expect(add('1'), equals(1));
-});
+import 'package:flutter_test/flutter_test.dart';
+import 'package:string_calculator_tdd/string_calculator.dart';
+
+void main() {
+  test('Single number returns value', () {
+    expect(add('1'), equals(1));
+  });
+
+  test('Two numbers return sum', () {
+    expect(add('1,5'), equals(6));
+  });
+
+}
